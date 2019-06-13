73b725f29a83fc8c8c58f3b80b02c429084987ef
Imed Adel <aadelimed@gmail.com>
2019-06-13 15:10:05 +0200

    :bug: clean up

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 chrome.browserAction.onClicked.addListener(tab => {
-	console.log(tab.url)
-	console.log(tab)
 	if (tab.url.substring(0, 16) !== `https://archive.`) {
 		chrome.tabs.update(tab.id, {
 			url: `https://archive.today/${new Date().getFullYear()}/${tab.url}`,
