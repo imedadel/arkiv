993b904fb9fa5f27a24f920487c7c859bbd2a0da
Imed Adel <aadelimed@gmail.com>
2019-06-13 14:59:29 +0200

    :sparkles: Allow go back to unarchived version

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 chrome.browserAction.onClicked.addListener(tab => {
 	console.log(tab.url)
 	console.log(tab)
-	chrome.tabs.update(tab.id, {
-		url: `https://archive.today/${new Date().getFullYear()}/${tab.url}`,
-	})
+	if (tab.url.substring(0, 16) !== `https://archive.`) {
+		chrome.tabs.update(tab.id, {
+			url: `https://archive.today/${new Date().getFullYear()}/${tab.url}`,
+		})
+	} else {
+		chrome.tabs.goBack(tab.id)
+	}
 })
